4a94d102b6ce4920ccc7969f89c05c72eb90594c
cavellerson <cavell.wong@gmail.com>
2025-05-30 22:47:35 -0400

    first commit

---
+++ src/app/aboutme/page.js
@@ -0,0 +1,14 @@
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae lectus non ante sagittis suscipit. Fusce id placerat odio, in varius elit. Etiam et pulvinar turpis. Nam ac porttitor metus. Proin ullamcorper metus nec blandit suscipit. Duis mollis tristique lacus eget tempus. Duis elementum ultricies bibendum.
+      </p>
+      <p>
+        Donec dignissim mattis erat, sit amet lacinia eros dapibus ac. Donec interdum tellus non mi fermentum varius. Fusce aliquet sollicitudin mi sed venenatis. Nunc id dui at risus ultricies blandit. Etiam iaculis dignissim vestibulum. Nulla ac felis mi. In hac habitasse platea dictumst. Morbi sed faucibus massa. Nam a mi varius, consequat libero sed, aliquet risus. Sed vulputate tristique dapibus. Aliquam eget lacinia lorem. Sed tempor sodales lectus, nec laoreet velit imperdiet in. Curabitur lacinia in orci at pharetra. Vivamus scelerisque quam et quam volutpat porta. Cras placerat turpis vitae pellentesque tincidunt.
+      </p>
+    </div>
+  );
+}
